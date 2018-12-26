54313a116fc910e174d3700de5b8332582fad63c
cesar augusto gadelha <cesar_ags@outlook.com>
2018-12-25 22:35:04 -0200

    inicial

---
+++ XGame/XGame.Domain/Entities/Jogador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace XGame.Domain.Entities
+{
+    public class Jogador
+    {
+        public Guid ID { get; set; }
+
+        public string PrimeiroNome { get; set; }
+        public string UltimoNome { get; set; }
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public string Status { get; set; }
+
+    }
+}
---
+++ XGame/XGame.Domain/Entities/MeusJogos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace XGame.Domain.Entities
+{
+    public class MeusJogo
+    {
+        public Guid ID { get; set; }
+
+        public JogoPlataforma JogoPlataforma { get; set; }
+        public bool Desejado { get; set; }
+        public bool Troca { get; set; }
+        public bool Vender { get; set; }
+        public DateTime DataDesejado { get; set; }
+    }
+}
